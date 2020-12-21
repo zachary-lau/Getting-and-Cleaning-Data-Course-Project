ac2751237a84aebab3d4ff778d2f6e2c84a72379
Zachary Lau <zachary.lauyatfung@gmail.com>
2020-12-21 15:50:40 +0800

    Create README.d

---
+++ README.d
@@ -0,0 +1,12 @@
+# Getting and Cleaning Data Course Project
+## Project Description
+The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set. The goal is to prepare tidy data that can be used for later analysis. You will be graded by your peers on a series of yes/no questions related to the project. You will be required to submit: 1) a tidy data set as described below, 2) a link to a Github repository with your script for performing the analysis, and 3) a code book that describes the variables, the data, and any transformations or work that you performed to clean up the data called CodeBook.md. You should also include a README.md in the repo with your scripts. This repo explains how all of the scripts work and how they are connected.
+
+## Files included in this repository:
+README.md - This README file for the repository
+CodeBook.md - A description of the data, variables, and transformations included
+run_analysis.R - The script use to generate the tidy dataset
+tidy_data.txt - The tidy dataset output of the included R script
+
+Using the run_analysis.R script:
+Simply download and run the source("run_analysis.R"). The script will proceed to download the original dataset. The final output of the script will be stored in the tidy_data.txt file.
